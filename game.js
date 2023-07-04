699ec3e48fc372e7a003760e8a51584a77fee6b7
Lionel NTIRUSHWA <linog02@gmail.com>
2023-07-04 10:35:04 +0200

    new files index.html and game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+    function getComputerChoice(){
+    const selection =["Rock", "Paper", "Scissors"];
+    const randomChoice = selection[Math.floor(Math.random() * 3)];
+        return randomChoice;
+    }
+
+   
